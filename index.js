b29ff6fd5a05518371c65acb663d274f413eccdd
Anoop Kumar <anoop_kr@yahoo.com>
2019-04-30 18:06:53 +0530

    TheO CLI learns/lists/partially forgets

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
+// ./bin/index.js
+
 const fs = require('fs')
-//const {list, learn} = require('./specials.js')
-var specials = require("./specials.json")
-
-const learn = (name, json) => {
-	console.log(">>>>>>>>>>>>>>>>>>", name, json)
-	//refresh(specials)
+var json = new Map()
+	
+const learn = (entity, name, type, tags) => {
+	json.entity = new Map()
+	json.entity.name = name
+	json.entity.type = type
+	json.entity.tags = tags
+	try{		
+		var specials = fs.readFileSync(entity + ".json")	
+		json = JSON.parse(specials)
+		refresh(entity)		
+		console.log("Updating ", json);
+	}
+	catch(err){
+		console.log("Creating a new ", entity);
+		refresh(entity)
+	}
 }
 
-const list = (name) => {
-	specials.map(special => console.log(Object.keys(special)[0], Object.values(special)[0]))
+const forget = (entity) => {
+	try{
+		fs.unlinkSync(entity + ".json")	
+	}
+	catch(err){
+		console.log("I do not know", entity, '.');
+		console.log("Teach me. Type --help");
+	}	
 }
 
-function refresh(specials){
-	fs.writeFileSync("./specials.json", JSON.stringify(specials))	
+const list = (entity, tags) => {
+	try{
+		var special = fs.readFileSync(entity + ".json")	
+		json = JSON.parse(special)
+		if(tags){
+			console.log(json.entity.tags)
+			return json.entity.tags
+		}
+		else console.log(json.entity)
+	}
+	catch(err){
+		console.log("I do not know", entity, '.');
+		console.log("Teach me. Type --help");
+	}
+}
+
+function refresh(entity){
+	fs.writeFileSync(entity +".json", JSON.stringify(json))	
 }
 
 const program = require('commander');
@@ -22,16 +57,22 @@ program
   .description('Node.js CLI for Brahma Services');
 
 program
-  .command('list [name]')
-  .alias('l')
-  .description('List all specials')
-  .action(name => list({name}))
+  .command('forget <Entity>')
+  .alias('f')
+  .description('Forgets a Special')
+  .action((entity) => forget(entity))
 
 program
-  .command('learn <name>')
-  .alias('r')
-  .description('Teaches the program new specials')
-  .action((name, json) => learn(name, json));
+  .command('list <Entity> [Name] Type] [Tags]')
+  .alias('l')
+  .description('List all specials on offer')
+  .action((entity, name, type, tags) => list(entity, name, type, tags))
+
+program
+  .command('learn <Entity> <Name> <Type> [Tags]')
+  .alias('k')
+  .description('Teaches theOracle new specials. Say entity (think of it as a unique pet name), name, type and optionally a set of comma separated Tags')
+  .action((entity, name, type, tags) => learn(entity, name, type, tags));
 
 program.parse(process.argv);
 
