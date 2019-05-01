dc07671f2fa34b1d9daa3e303c0dd60c58344730
anoopk <anoop_kr@yahoo.com>
2019-05-01 12:38:54 +0530

    Specials implementation moved in lib

--- bin/index.js
+++ bin/index.js
@@ -2,91 +2,20 @@
 
 const fs = require('fs')
 var json = new Map()
-	
-const learn = (entity, name, type, tags, image, text) => {	
-	if(null == image){
-		image = entity + ".jpg"
-	}
+const specials = require('../lib/specials.js') 
 
-	if(!text){
-		text = name
-	}
-	
-	json.entity = new Map()
-	json.entity.image = image
-	json.entity.text = text
-	json.entity.name = name
-	json.entity.type = type
-	json.entity.tags = tags	
-	try{		
-		var specials = fs.readFileSync(entity + ".json")	
-		json = JSON.parse(specials)
-		refresh(entity)		
-		console.log("Updating ", entity);
-	}
-	catch(err){
-		console.log("Creating a new ", entity);
-		create(entity)
-	}
+function list(entity, name, type, tags){
+	specials.list(entity, name, type, tags)
 }
 
-const forget = (entity) => {
-	try{
-		var knowledge = require('../knowledge.json')
-		knowledge.entities.splice(knowledge.entities.indexOf(entity), 1)
-		fs.writeFileSync('./knowledge.json', JSON.stringify(knowledge))	
-		
-		fs.unlinkSync(entity + ".json")	
-	}
-	catch(err){
-		console.log("I do not know", entity, '.');
-		console.log("Teach me. Type --help");
-	}	
+function learn(entity, name, type, tags, image, text){
+	specials.learn(entity, name, type, tags, image, text)
 }
 
-const listSpecials = () => {
-	try{
-		var knowledge = require('../knowledge.json')
-		if(knowledge.entities.length == 0){
-			assert(false)
-		}			
-		console.log(knowledge.entities)	
-	}
-	catch(err){
-		console.log("I have no Specials to offer. Teach me. Type theO --help to learn how.");
-	}
+function forget(entity){
+	specials.forget(entity)
 }
 
-const list = (entity, what) => {
-	if(entity){
-		try{
-			var special = fs.readFileSync(entity + ".json")	
-			json = JSON.parse(special)
-			if(what){
-				console.log(json.entity[what])
-				return json.entity.what
-			}
-			else console.log(json.entity)
-		}
-		catch(err){
-			console.log("I do not know", entity, '.');
-			console.log("Teach me. Type --help");
-		}
-	}
-	else listSpecials()
-}
-
-function create(entity){
-	var knowledge = require('../knowledge.json')
-	knowledge.entities.push(entity)
-	fs.writeFileSync('./knowledge.json', JSON.stringify(knowledge))
-	
-	fs.writeFileSync(entity +".json", JSON.stringify(json))	
-}
-
-function refresh(entity){
-	fs.writeFileSync(entity +".json", JSON.stringify(json))	
-}
 
 const program = require('commander');
 
@@ -101,17 +30,11 @@ program
   .action((entity) => forget(entity))
 
 program
-  .command('list [Entity] [Name] Type] [Tags]')
+  .command('list [Entity] [Name] [Type] [Tags]')
   .alias('l')
   .option('-s', '--specials', "Lists all the Special Entities")
   .description('Details of the Special')
   .action((entity, name, type, tags) => list(entity, name, type, tags))
-
-program
-  .command('listSpecials')
-  .alias('l')
-  .description('List all Specials')
-  .action(() => listSpecials())
 
 program
   .command('learn <Entity> <Name> <Type> [Tags] [Image] [Text]')
